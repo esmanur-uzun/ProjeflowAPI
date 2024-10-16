3b2de7d6208b975562fc0762026ca00273ed05d0
esmanur-uzun <esmabitmez@gmail.com>
2024-10-16 17:34:44 +0300

    worked on file upload

--- src/middlewares/lib/upload.ts
+++ src/middlewares/lib/upload.ts
@@ -1,3 +1,31 @@
-import multer from "multer";
+import multer,{FileFilterCallback} from "multer";
 import path from 'path';
 import fs from 'fs'; 
+import { Request,Response,  } from "express";
+
+const fileFilter = (req:Request,file: Express.Multer.File,cb:FileFilterCallback) =>{
+    const allowedMimeTypes = ["image/jpg","image/png","image/jpeg","image/svg"]
+
+    if(!allowedMimeTypes.includes(file.mimetype)){
+        cb(null,false)
+    }
+    cb(null,true)
+}
+
+const storage = multer.diskStorage({
+    destination: function(req:Request,file:Express.Multer.File,callback:(error: Error | null, destination: string) => void){
+        const rootDir = path.dirname(require.main?.filename || "")
+        const uploadPath = path.join(rootDir, "public/uploads/config");
+        fs.mkdirSync(uploadPath,{recursive:true})
+        callback(null,uploadPath)
+    },
+    filename: function(req:Request,file:Express.Multer.File,callback:(error: Error | null, destination: string) => void){
+        const extension = file.mimetype.split("/")[1]
+
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()  * 1E9)
+
+        let url = `image_${uniqueSuffix}.${extension}`
+
+        callback(null,url)
+    } 
+})
